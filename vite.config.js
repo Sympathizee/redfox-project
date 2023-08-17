592253c41c1804d7e9bfc17576df7471fc6af3ff
David Ferdinand Wiryawan <davidferdinandw@gmail.com>
2023-08-17 18:09:17 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/redfox-project/',
+  base: '/redfox/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
