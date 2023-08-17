e145a19daf670a787640f62787e8e54a57452146
David Ferdinand Wiryawan <davidferdinandw@gmail.com>
2023-08-17 14:37:14 +0700

    Update Arch.vue

--- src/views/Arch.vue
+++ src/views/Arch.vue
@@ -3,7 +3,7 @@
     <v-row no-gutters class="main-box" v-for="content in contents" :style="content.style">
       <v-col cols="3" class="left-box">
         <v-row no-gutters class="text-content position-relative h-100">
-          <v-col cols="12" class="px-3 pt-3 pb-0">
+          <v-col cols="12" class="px-3 pt-3 pb-0 text-right">
             <div class="text-name">
               {{ content.builder }}
             </div>
@@ -11,12 +11,12 @@
               {{ content.address }}
             </div>
             <div>
-              <v-chip v-for="concept in content.concept" size="small" class="text-name" variant="outlined">{{ concept }}</v-chip>
+              <v-chip v-for="concept in content.concept" size="small" class="text-name ml-2" variant="outlined">{{ concept }}</v-chip>
             </div>
           </v-col>
           <v-col class="position-absolute" cols="2" style="height: 5rem;bottom: 0; left: 0; border-left: 1px solid white;">
           </v-col>
-          <v-col cols="9" class="text-right pr-3 position-absolute" style="bottom: 0; right: 0;">
+          <v-col cols="12" class="pl-3 position-absolute" style="bottom: 0; right: 0;">
             {{ content.date }}
           </v-col>
         </v-row>
@@ -32,18 +32,20 @@
   import Carousel from '@/components/Carousel.vue';
 
   var contents = [
-  {
-      builder: 'Broghan Moura',
-      address: 'San Luis Ave. Fort Carson',
+    {
+      builder: 'Santiago Guerreira',
+      address: '202nd Mint Street',
       concept: ['Modern'],
       images: [
-        'https://cdn.discordapp.com/attachments/1137702625329029130/1137702867650760704/sa-mp-518_copy.jpg', 
-        'https://cdn.discordapp.com/attachments/1137702625329029130/1137702868011458620/sa-mp-513_copy.jpg', 
-        'https://cdn.discordapp.com/attachments/1137702625329029130/1137702868380561428/sa-mp-514_copy.jpg', 
-        'https://cdn.discordapp.com/attachments/1137702625329029130/1137702869194256474/sa-mp-516_copy.jpg', 
-        'https://cdn.discordapp.com/attachments/1137702625329029130/1137702869550780426/sa-mp-517_copy.jpg'
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870119745134672/samp_15_08_2023_11_36_49.png', 
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870120097468548/samp_15_08_2023_11_36_59.png', 
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870120625946684/samp_15_08_2023_11_37_20.png', 
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870121091518545/samp_15_08_2023_11_37_31.png', 
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870121540304917/samp_15_08_2023_11_37_57.png', 
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870121884241951/samp_15_08_2023_11_38_17.png',
+        'https://cdn.discordapp.com/attachments/1140870119099219999/1140870122236559390/samp_15_08_2023_11_38_56.png'
       ],
-      date: 'August, 6th 2023',
+      date: 'August, 15th 2023',
       style: {},
     },
     {
@@ -67,15 +69,29 @@
       concept: ['Modern'],
       images: [
         'https://cdn.discordapp.com/attachments/1137702625329029130/1137702867650760704/sa-mp-518_copy.jpg', 
-        'https://cdn.discordapp.com/attachments/1137702625329029130/1137702867650760704/sa-mp-518_copy.jpg', 
         'https://cdn.discordapp.com/attachments/1137702625329029130/1137702868011458620/sa-mp-513_copy.jpg', 
         'https://cdn.discordapp.com/attachments/1137702625329029130/1137702868380561428/sa-mp-514_copy.jpg', 
         'https://cdn.discordapp.com/attachments/1137702625329029130/1137702869194256474/sa-mp-516_copy.jpg', 
         'https://cdn.discordapp.com/attachments/1137702625329029130/1137702869550780426/sa-mp-517_copy.jpg'
       ],
       date: 'August, 6th 2023',
+      style: {},
+    },
+    {
+      builder: 'Broghan Moura',
+      address: 'San Luis Ave. Fort Carson',
+      concept: ['Modern'],
+      images: [
+        'https://cdn.discordapp.com/attachments/1137703910031753226/1137704148591181895/sa-mp-527_copy.jpg', 
+        'https://cdn.discordapp.com/attachments/1137703910031753226/1137704148947718184/sa-mp-528_copy.jpg', 
+        'https://cdn.discordapp.com/attachments/1137703910031753226/1137704149283241984/sa-mp-529_copy.jpg', 
+        'https://cdn.discordapp.com/attachments/1137703910031753226/1137704149748830238/sa-mp-530_copy.jpg', 
+        'https://cdn.discordapp.com/attachments/1137703910031753226/1137704150185017344/sa-mp-531_copy.jpg', 
+        'https://cdn.discordapp.com/attachments/1137703910031753226/1137704150495399986/sa-mp-532_copy.jpg'
+      ],
+      date: 'August, 6th 2023',
       style: {borderBottom:'1px solid white'},
-    }
+    },
   ]
 
   function wheel(event) {
